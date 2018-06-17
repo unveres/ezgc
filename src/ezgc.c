d6ec5c5e89d54e626e802ccfc2862a88c9ab12e8
Mateusz Ezechiel Górka <ez@localhost.localdomain>
2018-06-18 00:25:30 +0200

    comment

--- src/ezgc.c
+++ src/ezgc.c
@@ -20,6 +20,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <string.h>
 #include <ezgc.h>
 
+/* ignore inline ANSI C */
 #if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
 #  define inline
 #endif
